2c98720557759dbc5964e486a7cb10aa1b493c9e
Schluterman/laptop <60680674+Schluterman@users.noreply.github.com>
2022-09-02 15:39:28 -0500

    day12 lvl 1 complete

---
+++ 12_Day_Modules/day12.py
@@ -0,0 +1,29 @@
+#1
+import random
+import string
+
+# def random_user_id():
+#     identity = ''
+#     for _ in range(6):
+#         identity += random.choice(string.ascii_letters)
+#     return identity
+
+
+# print(random_user_id())
+
+#2
+# def user_id_gen_by_user():
+#     num_chars = int(input('number of characters '))
+#     ids = int(input('number of ids needed to generate '))
+#     for _ in range(ids):
+#         identify= ''.join([random.choice(string.ascii_letters) for _ in range(num_chars)])
+#         print(identify)
+# print(user_id_gen_by_user())
+
+#3 
+# def rgb_color_gen():
+#     r = str(random.randint(0,255))
+#     g = str(random.randint(0,255))
+#     b = str(random.randint(0,255))
+#     return "rgb(" + r + "," + g + "," + b + ")"
+# print(rgb_color_gen())
