b639fc206126313296504694dba536bd419c0358
Schluterman <60680674+Schluterman@users.noreply.github.com>
2022-09-06 09:13:19 -0500

    start day 14

---
+++ 14_Day_Higher_order_functions/day14.py
@@ -0,0 +1,53 @@
+#High Order Functions 
+from functools import reduce
+from re import I
+
+
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#LVL ONE
+#1 
+#The map() function is a built-in function that takes a function and iterable as parameters.
+#The Map actually only iterates over a list 
+#The filter() function calls the specified function which returns boolean for each item of the specified iterable (list). It filters the items that satisfy the filtering criteria.
+#The reduce() function does not return another iterable, instead it returns a single value
+
+#2 Explain the difference between higher order function, closure and decorator
+#higher order function
+# A function can take one or more functions as parameters
+# A function can be returned as a result of another function
+# A function can be modified
+# A function can be assigned to a variable
+#Closure  nested function to access the outer scope
+#decorator  A decorator is a design pattern in Python that allows a user to add new functionality to an existing object while  accepting new parameters
+
+#3 Define a call function before map, filter or reduce, see examples
+def cube(num):
+    return num ** 3
+
+
+def vowel_name(name):
+    if name[0] in 'aeiouAEIOU':
+        return True
+    return False
+
+
+def sum_of_cubes(num1, num2):
+    return num1 + num2
+
+print(list(filter(vowel_name, names)))
+print(list(map(cube, numbers)))
+print(reduce(sum_of_cubes, list(map(cube, numbers))))
+
+#4
+for i in countries:
+    print(i)
+#5
+for i in names:
+    print(i)
+#6
+for i in numbers:
+    print(i)
+
+
